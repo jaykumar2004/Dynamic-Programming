13d60cb379dccf9d9c3bc60921b74a40a9303468
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-14 15:27:04 +0530

    Time: 47 ms (67.35%) | Memory: 54.8 MB (12.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,15 +2,11 @@
  * @param {number} n
  * @return {number}
  */
-
-let store = {}
 var fib = function (n) {
-    if (n <= 1) {
-        return n;
-    };
-    if (!store[n]) {
-        store[n] = fib(n - 1) + fib(n - 2);
-    }
+    let dp = [0, 1];
 
-    return store[n];
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
 };
